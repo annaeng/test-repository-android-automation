5e9dc7b4ea4d99e99466d3ba92511fdbc4567d66
annaeng <engel.anna@gmx.ch>
2018-04-29 18:28:00 +0200

    Accertions for all tests

--- area screen steps.rb
+++ area screen steps.rb
@@ -11,26 +11,44 @@ Then(/^I land on Area screen$/) do
 end
 
 When(/^I click on Swap button$/) do
-  puts ("User clicks Swap button")
+  find_element(id:"fab").click
 end
 
 Then(/^I see "([^"]*)" in From header$/) do |value|
-  puts("From header values is " + value)
+  actual_value = find_element(id:"header_text_unit_from").text
+  if actual_value !=value
+    fail("Expected value is #{value}, but actual value was #{actual_value}")
+  end
 end
 
-
 And(/^I see "([^"]*)" in To header$/) do |value|
-  puts("To header values is #{value}")
+  actual_value = find_element(id:"header_text_unit_to").text
+  if actual_value !=value
+    fail("Expected value is #{value}, but actual value was #{actual_value}")
+  end
 end
 
 And(/^I click on Clear button$/) do
-  puts("User clicks on Clear button")
+  find_element(id:"menu_clear").click
 end
 
 When(/^I enter "([^"]*)" to From field$/) do |value|
-  puts("User entered value #{value}")
+  find_element(id:"header_value_from").send_keys(value)
 end
 
 Then(/^I get "([^"]*)" in To field$/) do |value|
-  puts("User sees #{value} in result field")
+  actual_value = find_element(id:"header_value_to").text
+  if actual_value !=value
+    fail("Expected value is #{value}, but actual value was #{actual_value}")
+  end
+end
+
+
+When(/^I click on From field$/) do
+  find_element(id:"header_value_from").click
+end
+
+And(/^I press "([^"]*)" on soft keyboard$/) do |value|
+  digit = Integer(value)
+  press_keycode 7 + digit
 end
